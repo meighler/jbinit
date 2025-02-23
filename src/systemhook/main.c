8615b754ee0365b96605237c917308c8b282b7d1
Nick Chan <towinchenmi@gmail.com>
2025-02-23 15:56:49 +0800

    HACK: disable sandbox_init_with_extensions_hook
    iPadOS 18.4 removed it

--- src/systemhook/main.c
+++ src/systemhook/main.c
@@ -639,7 +639,8 @@ DYLD_INTERPOSE(execvp_hook, execvp)
 DYLD_INTERPOSE(execvP_hook, execvP)
 DYLD_INTERPOSE(sandbox_init_hook, sandbox_init)
 DYLD_INTERPOSE(sandbox_init_with_parameters_hook, sandbox_init_with_parameters)
-DYLD_INTERPOSE(sandbox_init_with_extensions_hook, sandbox_init_with_extensions)
+
+//DYLD_INTERPOSE(sandbox_init_with_extensions_hook, sandbox_init_with_extensions)
 DYLD_INTERPOSE(reboot3_hook, reboot3)
 
 __API_AVAILABLE(macos(10.15), ios(13.0), tvos(13.0), bridgeos(4.0))
